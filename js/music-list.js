82b03af95a9f5a119ea966a9188a83355f903f9f
summer2059 <143068765+summer2059@users.noreply.github.com>
2023-08-24 14:40:19 +0545

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,46 @@
+
+
+let allMusic = [
+  {
+    name: "Narunu Aama",
+    artist: "Rahul Malla Thakuri",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Flame",
+    artist: "Zayn & Jungleboi",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Aama",
+    artist: "Sushant KC",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Kasari Bhanu",
+    artist: "Swoopna Suman",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Koi Puche Mere Dil Se",
+    artist: "Sahir Ali Bagga",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Awara",
+    artist: "Salman Ali",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Ek Number",
+    artist: "Himesh Reshammiya",
+    img: "music-7",
+    src: "music-7"
+  },
+];
